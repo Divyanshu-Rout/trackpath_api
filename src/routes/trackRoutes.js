45173818650e3502cacf95eccc993772b979762f
Divyanshu-Rout <divyanshukumarrout@gmail.com>
2022-06-20 13:34:04 +0530

    added post req

--- src/routes/trackRoutes.js
+++ src/routes/trackRoutes.js
@@ -14,4 +14,22 @@ router.get('/tracks', async (req, res) => {
     res.send(tracks);
 })
 
+router.post('/tracks', async (req, res) => {
+    const { name, locations } = req.body;
+    if (!name || !locations) {
+        return res.status(422).send({ error: 'You must provide a name and locations' });
+    }
+
+    const track = new Track({ name: name, locations: locations, userId: req.user._id })
+
+    try {
+        await track.save();
+        res.send(track);
+    } catch (e) {
+        res.status(422).send({ error: e.message });
+    }
+
+
+})
+
 module.exports = router;
